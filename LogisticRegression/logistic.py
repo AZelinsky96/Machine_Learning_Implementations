53399e7f46b351e997eef32293cb0802f3fd3911
AZelinsky96 <azelinsky1996@gmail.com>
2019-06-02 23:00:08 -0400

    Building the ML Repo and Started Log

---
+++ LogisticRegression/logistic.py
@@ -0,0 +1,16 @@
+import pandas as  pd
+import numpy as np
+import os
+
+import sys
+
+sys.path.append("/home/zeski/Documents/PythonLessons/MLImplementations/Classes")
+
+import Regression_Class as RegClass
+
+
+def main():
+    return None
+
+
+print(main())
